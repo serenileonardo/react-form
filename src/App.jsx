ccc8b95836fa93b0ddbd5eafb3edcf0f2a400b90
serenileonardo <serenileonardo.web@gmail.com>
2026-01-22 16:03:58 +0100

    feat: created array articles and put map to print

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
 import { useState } from 'react'
 
+
+
 function App() {
+
+  const [articles, setArticles] = useState([
+    { id: 1, title: "React è da pazzi" },
+    { id: 2, title: "Forza Roma" },
+    { id: 3, title: "divina commedia" }
+  ]);
+
   return (
     <div>
-      <h1>Blog</h1>
+      <h1>ARTICOLI</h1>
+
+      <ul>
+        {articles.map((article) => (
+          <li key={article.id}>
+            {article.title}
+          </li>
+
+        ))}
+
+      </ul>
     </div>
-  )
+
+  );
 }
 
-
 export default App
+
+
+
