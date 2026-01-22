c76e8dcc91d21076e78bf9b8b7925f024a83e901
serenileonardo <serenileonardo.web@gmail.com>
2026-01-22 17:18:44 +0100

    feat: push

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import { useState } from 'react'
 
-
+const originalArticles = [
+  { id: 1, title: "React è da pazzi" },
+  { id: 2, title: "Forza Roma" },
+  { id: 3, title: "divina commedia" }
+]
 
 function App() {
 
-  const [articles, setArticles] = useState([
-    { id: 1, title: "React è da pazzi" },
-    { id: 2, title: "Forza Roma" },
-    { id: 3, title: "divina commedia" }
-  ]);
+  const [articles, setArticles] = useState(originalArticles);
+  const [newTitle, setNewTitle] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault() // refresh della pagina
+
+    if (newTitle === "") return
+
+    const newArticle = {
+      id: articles.length + 1, //Serve per creare un id nuovo.
+      title: newTitle
+    }
+
+    setArticles([...articles, newArticle])  // aggiorna lista
+    setNewTitle("") // svuota l'input
+
+  }
 
   return (
     <div>
@@ -19,14 +35,25 @@ function App() {
           <li key={article.id}>
             {article.title}
           </li>
-
         ))}
-
       </ul>
-    </div>
 
-  );
+
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={newTitle} // “Il valore nell’input è sempre quello che c’è nello state newTitle
+          onChange={(e) => setNewTitle(e.target.value)} //Salva quello che l’utente ha scritto nello state
+          placeholder="Nuovo articolo"
+        />
+
+        <button type="submit">Aggiungi</button>
+      </form>
+    </div>
+  )
 }
+
 
 export default App
 
